d36af65d93eb84837383ca397f98a80fb725a503
OmgValicious <ziallshamrock@gmail.com>
2022-09-15 10:06:21 -0700

    Route added to search gifs and page layout

--- controllers/gifs.js
+++ controllers/gifs.js
@@ -1,21 +1,19 @@
 const express = require('express')
-const { Model } = require('sequelize')
 const router = express.Router()
 const db = require('../models')
+const axios = require('axios')
 
 //////////////////////////////////////////////////
 //ROUTES
 //////////////////////////////////////////////////
 
 router.get('/', (req, res) => {
-    axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=18&rating=g`)
+    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${req.query.gifSearch}&limit=5&rating=g&lang=en`)
     .then(response => {
-        response.data.data.forEach(item => {console.log(item.images.original.url)})
-        res.render('home.ejs', { gifs: response.data.data })
-        console.log('the currently logged in user is:', res.locals.user)
+        // response.data.data.forEach(item => {console.log(item.images.original.url)})
+        res.render('gifs/search.ejs', { gifs: response.data.data })
     })
-        .catch(console.log)
-      
+        .catch(console.log) 
 })
 
 
--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use(async (req, res, next) => {
 app.get('/', (req, res) => {
     axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=18&rating=g`)
     .then(response => {
-        response.data.data.forEach(item => {console.log(item.images.original.url)})
+        // response.data.data.forEach(item => {console.log(item.images.original.url)})
         res.render('home.ejs', { gifs: response.data.data })
         console.log('the currently logged in user is:', res.locals.user)
     })
